f3a6b7ded8fdd13d115c4faf4b154a5f9f21b371
Tanishq Raj <Tanishqraj306@gmail.com>
2025-11-11 13:54:42 +0530

    Add repositories page and route

---
+++ frontend-service/src/pages/RepositoriesPage.tsx
@@ -0,0 +1,9 @@
+export function RepositoriesPage() {
+  return (
+    <div className="w-full">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-3xl font-bold">Repositories</h1>
+      </div>
+    </div>
+  )
+}
